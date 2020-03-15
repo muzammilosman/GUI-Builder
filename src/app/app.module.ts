36f9c804458ae9a23e456647538a7e81b083c04b
muzammilosman <usman.muzammil@gmail.com>
2020-03-15 22:54:52 +0530

    Layout and material css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
+// Angular Material Modules
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+// Components
 import { WorkboardElementComponent } from './components/workboard-element/workboard-element.component';
 import { WorkboardComponent } from './components/workboard/workboard.component';
 import { ElementDetailComponent } from './components/element-detail/element-detail.component';
 
+// Services
 import { StorageService } from './services/storage.service';
 
 
@@ -27,7 +38,11 @@ import { StorageService } from './services/storage.service';
     BrowserAnimationsModule,
     DragDropModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule
   ],
   providers: [
     StorageService
